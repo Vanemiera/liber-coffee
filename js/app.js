3971c73033f7b5df5ed3b92d0bad5dc2113a212d
Vanemiera <vanemiera@gmail.com>
2016-04-26 16:48:28 +0200

    add basic randomizing

--- js/app.js
+++ js/app.js
@@ -1,9 +1,78 @@
 'use strict';
 
+var allPerks = [
+  {name: "Laser Sight"},
+  {name: "Cardio Accelerator"},
+  {name: "Auto-Injector"},
+  {name: "Strongarm"},
+  {name: "Smoke Grenades"}
+];
+
+var allPrimaries = [
+  {name: "Liberator"},
+  {name: "Justice"},
+  {name: "Breaker"},
+  {name: "Punisher"},
+  {name: "Double Freedom"}
+];
+
+var allStratagems = [
+  {name: "Turret"},
+  {name: "Mech"},
+  {name: "Machine Gun"},
+  {name: "Ammo"},
+  {name: "Vindicator"}
+];
+
+var getRandomPerk = function() {
+  return allPerks[Math.floor(Math.random()*allPerks.length)];
+};
+
+var getRandomPrimary = function() {
+  return allPrimaries[Math.floor(Math.random()*allPrimaries.length)];
+};
+
+var getRandomStratagem = function(slot) {
+  var rndStrat = allStratagems[Math.floor(Math.random()*allStratagems.length)];
+  var newStrat = {slot: slot, name: rndStrat.name};
+  return newStrat;
+};
+
 angular.module('liber-coffee', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/test'});
 }])
-.controller('LoadoutCtrl', function(){
+.controller('LoadoutCtrl', function($scope){
+  $scope.perk = getRandomPerk();
+  $scope.rollPerk = function() {
+    $scope.perk = getRandomPerk();
+  };
+
+  $scope.primary = getRandomPrimary();
+  $scope.rollPrimary = function() {
+    $scope.primary = getRandomPrimary();
+  };
+
+  $scope.stratagems = [
+    getRandomStratagem(0),
+    getRandomStratagem(1),
+    getRandomStratagem(2),
+    getRandomStratagem(3)
+  ];
+  $scope.rollStratagem = function(index) {
+    $scope.stratagems[index] = getRandomStratagem(index);
+  };
+  $scope.rollallStratagems = function() {
+    for(var i=0; i < $scope.stratagems.length; i++) {
+      $scope.stratagems[i] = getRandomStratagem(i);
+    }
+  };
+
+  $scope.rollEverything = function() {
+    $scope.rollPerk();
+    $scope.rollPrimary();
+    $scope.rollallStratagems();
+  }
+
 
 });
