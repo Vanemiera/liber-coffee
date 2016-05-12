87c9d1d10959291727c71674cb6d88a982b44c4f
Philipp Westermann <vanemiera@gmail.com>
2016-05-12 12:14:16 +0200

    Refactoring

--- karma.conf.js
+++ karma.conf.js
@@ -4,8 +4,6 @@ module.exports = function(config){
     basePath : './',
 
     files : [
-      'node_modules/angular/angular.js',
-      'node_modules/angular-route/angular-route.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'js/*.js'
     ],
